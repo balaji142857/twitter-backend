2a0174edaca4bf3c226acd8096df1c50b737c72f
Balaji Krishnan <balaji2k9_srec14@ymail.com>
2021-04-24 23:20:35 +0530

    model mapper introduced

--- tweet-service/src/main/java/com/kb/twitter/service/impl/TweetServiceImpl.java
+++ tweet-service/src/main/java/com/kb/twitter/service/impl/TweetServiceImpl.java
@@ -9,6 +9,7 @@ import java.util.stream.Collectors;
 import java.util.stream.StreamSupport;
 
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.kb.twitter.entity.Tweet;
 import com.kb.twitter.repos.TweetRepository;
@@ -23,6 +24,7 @@ public class TweetServiceImpl implements TweetService {
 	private TweetRepository repo;
 
 	@Override
+	@Transactional(readOnly = true, rollbackFor = Exception.class)
 	public List<Tweet> list() {
 		//TODO use entity mappers -- entity is now sent directly
 		Iterable<Tweet> iterable = repo.findAll();
@@ -33,16 +35,19 @@ public class TweetServiceImpl implements TweetService {
 	}
 
 	@Override
+	@Transactional(rollbackFor = Exception.class)
 	public Tweet create(Tweet tweet) {
 		return repo.save(tweet);
 	}
 
 	@Override
+	@Transactional(rollbackFor = Exception.class)
 	public void delete(Long id) {
 		repo.deleteById(id);
 	}
 
 	@Override
+	@Transactional(readOnly = true, rollbackFor = Exception.class)
 	public Optional<Tweet> get(Long id) {
 		return repo.findById(id);
 	}
